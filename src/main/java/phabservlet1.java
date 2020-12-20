91ed96385bf4a2210033357ea09a6a5be0449e15
maciej-123 <maciejzajaczkowskiscience@gmail.com>
2020-12-20 08:47:32 +0000

    v1.2 test database for heroku postgres

--- src/main/java/phabservlet1.java
+++ src/main/java/phabservlet1.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.stream.Collectors;
@@ -27,6 +29,9 @@ public class phabservlet1 extends HttpServlet {
 //            convertFiletoString FtoS = new convertFiletoString(sqlfile);
 //            resp.getWriter().write(FtoS.getStringOutput());
 
+
+
+
               resp.getWriter().write(" TEST ");
 
         }
@@ -35,6 +40,51 @@ public class phabservlet1 extends HttpServlet {
             resp.getWriter().write("<b>Drug Stock</b>");
             resp.getWriter().write(" TEST ");
         }
+
+
+
+        Connection c=null;
+        Statement s=null;
+        ResultSet rset=null;
+        try {
+            c= DriverManager.getConnection("JDBC_DATABASE_URL");
+            s=c.createStatement();
+            //select table from INFORMATION_SCHEMA.TABLES - lis of all the tables
+            String strSelect = "SELECT * \n" +
+                    "                 FROM INFORMATION_SCHEMA.TABLES \n" +
+                    "                 WHERE  TABLE_NAME = test_database\n";
+
+
+
+            rset = s.executeQuery(strSelect);
+            if (!rset.next()){
+                System.out.println("Creating table");
+                s.execute("create table test_database\n" +
+                        "(\n" +
+                        "    one   serial       not null\n" +
+                        "        constraint test_database_pkey\n" +
+                        "            primary key,\n" +
+                        "    two   varchar(128) not null,\n" +
+                        "    three varchar(128) not null\n" +
+                        ");\n" +
+                        "\n" +
+                        "alter table test_database\n" +
+                        "    owner to postgres;\n" +
+                        "\n" +
+                        "INSERT INTO public.test_database (one, two, three) VALUES (1, '2', '3');\n" +
+                        "INSERT INTO public.test_database (one, two, three) VALUES (4, '5', '6');\n" +
+                        "INSERT INTO public.test_database (one, two, three) VALUES (7, '8', '9');");
+            }
+            else {
+                /*strSelect = "ALTER TABLE "+shapeName+"s ADD COLUMN id INT";
+                rset = s.executeQuery(strSelect);*/
+                System.out.println("table exists");
+            }
+        }
+        catch (Exception e){
+            System.err.println(e.getMessage());
+        }
+
 
 
 
