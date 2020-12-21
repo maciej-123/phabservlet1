bd83e979ef0de41c3d9a6528b5a933ba39b3375b
maciej-123 <maciejzajaczkowskiscience@gmail.com>
2020-12-21 15:07:20 +0000

    v2.6

--- src/main/java/phabservlet1.java
+++ src/main/java/phabservlet1.java
@@ -20,12 +20,8 @@ public class phabservlet1 extends HttpServlet {
 
         resp.setContentType("test");
 
+        try { Class.forName("org.postgresql.Driver"); } catch (Exception e) {resp.getWriter().write(e.getMessage()); }
         try { c = DriverManager.getConnection("JDBC_DATABASE_URL"); } catch (Exception e) {resp.getWriter().write(e.getMessage());}
-        try {
-            // Registers the driver
-            Class.forName("org.postgresql.Driver");
-        } catch (Exception e) {resp.getWriter().write(e.getMessage());
-        }
 
         String ending = req.getServletPath();
         if(ending.equals("/text_database")) {
