2148337ea17a1443db939ad3bdb60e4f198d0a91
maciej-123 <maciejzajaczkowskiscience@gmail.com>
2020-12-23 17:20:00 +0000

    v7.10

--- src/main/java/phabservlet1.java
+++ src/main/java/phabservlet1.java
@@ -122,6 +122,31 @@ public class phabservlet1 extends HttpServlet {
 //            resp.getWriter().write(LengthManufacturer);
 
 
+            try {
+
+                resp.getWriter().write("Editing Rows Paddington\n");
+                Statement s=c.createStatement();
+
+
+
+                //s.execute("UPDATE public.StockDBPaddington SET CurrentStock = 500 WHERE Name = 'tabs                ';");
+
+                s.execute("UPDATE public.StockDBPaddington SET CurrentStock = 400 WHERE Name = "+SearchName+";");
+
+                resp.getWriter().write("\nDecrease Stock Called");
+                if(s!=null){s.close();}
+
+                SearchName = "";
+                SearchManufacturer = "";
+
+
+            }
+            catch (Exception e){
+
+                resp.getWriter().write(e.getMessage());
+            }
+
+
         }
 
 
