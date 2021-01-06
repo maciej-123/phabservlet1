423aef0d645db1439f574e4cb2b19edb64db06ff
Aaronlyl <leeaaronyl@gmail.com>
2021-01-06 17:13:10 +0000

    added user database

--- src/main/java/phabservlet1.java
+++ src/main/java/phabservlet1.java
@@ -47,6 +47,11 @@ import javax.servlet.http.*;
 
 
 
+                //URL patterns for user database
+                "/create_user_database",
+                "/return_user_database",
+                "/add_user",
+                "/verify_user",
                 //new URL patterns for post requests
 
                 //not important - `this is just to create a test database
@@ -229,11 +234,21 @@ public class phabservlet1 extends HttpServlet {
             alterTestDatabase(resp);
         }
 
+        //User database functions
+        if(urlPattern.equals("/create_user_database")) {
+            createUserDatabase(resp);
+        }
+        if(urlPattern.equals("/return_user_database")) {
+            returnUserDatabse(resp);
+        }
+        
 
         ///NEED TO ADD
         //post request for warning - will check if blow 20% for everydrug - will be run after doing any other post requests
         //add to identical databases for 2 other branches
         //have an automatic revenue and profit function for all branches. - remember to edit selling price
+
+
 
 
     }
@@ -299,6 +314,62 @@ public class phabservlet1 extends HttpServlet {
                 resp.getWriter().write(e.getMessage());
             }
             resp.getWriter().write("Input Branch ended");
+        }
+
+        
+
+        //message in the format of firstname/lastname|username@password#email
+
+        if(urlPattern.equals("/add_user")) {
+            try {
+                Statement s = c.createStatement();
+                String firstname = message.substring(0,message.indexOf('/'));
+                String lastname = message.substring(message.indexOf('/')+1,message.indexOf('|'));
+                String username = message.substring(message.indexOf('|')+1,message.indexOf('@'));
+                String password = message.substring(message.indexOf('@')+1,message.indexOf('#'));
+                String email    = message.substring(message.indexOf('#')+1,length);
+
+                String usrSel = "SELECT Username from Users WHERE Username = "+username;
+                ResultSet rset = s.executeQuery(usrSel);
+
+                if(rset.getString("Username").equals(username)) {
+                    resp.getWriter().write("username unavailable");
+                }
+                else {
+                    resp.getWriter().write("username available");
+                    
+                    resp.getWriter().write("\n");
+                    resp.getWriter().write(firstname);
+                    resp.getWriter().write("\n");
+                    resp.getWriter().write(lastname);
+                    resp.getWriter().write("\n");
+                    resp.getWriter().write(username);
+                    resp.getWriter().write("\n");
+                    resp.getWriter().write(email);
+
+                    s.execute("INSERT INTO Users" + "(Username, FirstName, LastName, Email, Password) VALUES ("
+                            +"'"+username+"'"+"'"+firstname+"'"+"'"+lastname+"'"+"'"+email+"'"+"'"+password+"'");
+
+                    resp.getWriter().write("Inserted user: ");
+                    resp.getWriter().write(username);
+                }
+            }
+            catch(Exception e) {
+                resp.getWriter().write(e.getMessage());
+            }
+
+
+
+        }
+        if(urlPattern.equals("/verify_user")) {
+            try {
+                
+
+            }
+
+            catch(Exception e) {
+                resp.getWriter().write(e.getMessage());
+            }
         }
     }
 
@@ -922,6 +993,37 @@ public class phabservlet1 extends HttpServlet {
             resp.getWriter().write("Creating PHAB Database for Green Park\n");
             Statement s=c.createStatement();
 
+            boolean lockCreate = false;
+            //fill database with test rot
+
+            if(lockCreate == true)
+            {
+                resp.getWriter().write("Creation Locked, Please Edit code\n");
+            }
+            else {
+                //Cold and Flu
+                s.execute("INSERT INTO public.StockDBGreenPark (Manufacturer,Name,Quantity,SalesPrice,PurchasePrice,FullStock,LimitOne,CurrentStock) VALUES ('vicks','vaporub','100g',9,3.7,15,0,15)");
+                s.execute("INSERT INTO public.StockDBGreenPark (Manufacturer,Name,Quantity,SalesPrice,PurchasePrice,FullStock,LimitOne,CurrentStock) VALUES ('vicks','first defence','15ml',13.6,5,20,0,20)");
+                s.execute("INSERT INTO public.StockDBGreenPark (Manufacturer,Name,Quantity,SalesPrice,PurchasePrice,FullStock,LimitOne,CurrentStock) VALUES ('gsk','night nurse','160ml',17,7,30,0,0)");//actual 30
+                s.execute("INSERT INTO public.StockDBGreenPark (Manufacturer,Name,Quantity,SalesPrice,PurchasePrice,FullStock,LimitOne,CurrentStock) VALUES ('gsk','night nurse','160ml',18,7.5,30,0,30)");//actual 30
+                s.execute("INSERT INTO public.StockDBGreenPark (Manufacturer,Name,Quantity,SalesPrice,PurchasePrice,FullStock,LimitOne,CurrentStock) VALUES ('lemsip','max','16 caps',8.4,3.7,25,0,25)");
+                s.execute("INSERT INTO public.StockDBGreenPark (Manufacturer,Name,Quantity,SalesPrice,PurchasePrice,FullStock,LimitOne,CurrentStock) VALUES ('lemsip','standard','10 sachets',9,3.5,25,0,25)");
+                s.execute("INSERT INTO public.StockDBGreenPark (Manufacturer,Name,Quantity,SalesPrice,PurchasePrice,FullStock,LimitOne,CurrentStock) VALUES ('sudafed','day and night','16 caps',9,3.2,30,1,30)");
+                s.execute("INSERT INTO public.StockDBGreenPark (Manufacturer,Name,Quantity,SalesPrice,PurchasePrice,FullStock,LimitOne,CurrentStock) VALUES ('sudafed','max','16 caps',8.4,3.2,30,1,30)");
+                s.execute("INSERT INTO public.StockDBGreenPark (Manufacturer,Name,Quantity,SalesPrice,PurchasePrice,FullStock,LimitOne,CurrentStock) VALUES ('benylin','mucus relief','16 caps',9.6,3.2,20,0,20)");
+                s.execute("INSERT INTO public.StockDBGreenPark (Manufacturer,Name,Quantity,SalesPrice,PurchasePrice,FullStock,LimitOne,CurrentStock) VALUES ('benylin','4 flu','24 caps',12,4.9,20,0,20)");
+
+                //Skincare
+                s.execute("INSERT INTO public.StockDBGreenPark (Manufacturer,Name,Quantity,SalesPrice,PurchasePrice,FullStock,LimitOne,CurrentStock) VALUES ('e45','psoriasis cream','50ml',40,16,15,0,15)");
+                s.execute("INSERT INTO public.StockDBGreenPark (Manufacturer,Name,Quantity,SalesPrice,PurchasePrice,FullStock,LimitOne,CurrentStock) VALUES ('eurax','skin cream','100g',11.4,4.2,15,0,15)");
+                s.execute("INSERT INTO public.StockDBGreenPark (Manufacturer,Name,Quantity,SalesPrice,PurchasePrice,FullStock,LimitOne,CurrentStock) VALUES ('eucerin','skin relief cream','50ml',18,7,20,0,20)");
+                s.execute("INSERT INTO public.StockDBGreenPark (Manufacturer,Name,Quantity,SalesPrice,PurchasePrice,FullStock,LimitOne,CurrentStock) VALUES ('eucerin','face scrub','100ml',15,6,20,0,20)");
+                s.execute("INSERT INTO public.StockDBGreenPark (Manufacturer,Name,Quantity,SalesPrice,PurchasePrice,FullStock,LimitOne,CurrentStock) VALUES ('dermalex','psoriasis cream','150ml',60,25,10,0,10)");
+                s.execute("INSERT INTO public.StockDBGreenPark (Manufacturer,Name,Quantity,SalesPrice,PurchasePrice,FullStock,LimitOne,CurrentStock) VALUES ('dermalex','repair and Restore','100g',24,10,10,0,10)");
+                s.execute("INSERT INTO public.StockDBGreenPark (Manufacturer,Name,Quantity,SalesPrice,PurchasePrice,FullStock,LimitOne,CurrentStock) VALUES ('dermalex','eczema cream','30g',24,9.7,20,0,20)");
+                s.execute("INSERT INTO public.StockDBGreenPark (Manufacturer,Name,Quantity,SalesPrice,PurchasePrice,FullStock,LimitOne,CurrentStock) VALUES ('dermalex','eczema cream','100g',50,22.2,5,0,5)");
+                s.execute("INSERT INTO public.StockDBGreenPark (Manufacturer,Name,Quantity,SalesPrice,PurchasePrice,FullStock,LimitOne,CurrentStock) VALUES ('cetaphil','moisturising cream','50ml',20,7.6,20,0,20)");
+                s.execute("INSERT INTO public.StockDBGreenPark (Manufacturer,Name,Quantity,SalesPrice,PurchasePrice,FullStock,LimitOne,CurrentStock) VALUES ('cetaphil','exfoliating cleanser','180ml',24,10.1,20,0,20)");
             //select table from INFORMATION_SCHEMA.TABLES - list of all the tables
             String strSelect = "SELECT * FROM INFORMATION_SCHEMA.TABLES";
 
@@ -944,7 +1046,7 @@ public class phabservlet1 extends HttpServlet {
             resp.getWriter().write("Function Call Finished");
             if(rset!=null){rset.close();}
             if(s!=null){s.close();}
-        }
+        }}
         catch (Exception e){
 
             resp.getWriter().write(e.getMessage());
@@ -1188,4 +1290,74 @@ public class phabservlet1 extends HttpServlet {
         }
 //
     }
+
+    private void createUserDatabase(HttpServletResponse resp) throws IOException {
+        try{
+            resp.getWriter().write("Creating User Database\n");
+            Statement s = this.c.createStatement();
+
+            String strSelect = "SELECT * FROM INFORMATION_SCHEMA.TABLES";
+
+            ResultSet rset = s.executeQuery(strSelect);
+
+            s.execute("CREATE TABLE Users(\n" +
+                "Username varchar(50) NOT NULL," +
+                "FirstName varchar(100) NOT NULL," +
+                "LastName varchar(100) NOT NULL," +
+                "Email varchar(100) NOT NULL" +
+                "Password varchar(100) NOT NULL"
+                
+            );
+
+            resp.getWriter().write("Function Call Finished");
+            if(rset!=null){rset.close();}
+            if(s!=null){s.close();}
+
+        } 
+        
+        
+        catch (Exception e) {
+            resp.getWriter().write(e.getMessage());
+
+        }  
+    }
+
+    
+    private void returnUserDatabse(HttpServletResponse resp) throws IOException {
+        try{
+            resp.getWriter().write("User Database\n");
+            Statement s = this.c.createStatement();
+            String strSelect = "SELECT * FROM Users";
+            
+            ResultSet rset = s.executeQuery(strSelect);
+
+            resp.getWriter().write("Table Start");
+
+            ResultSetMetaData rsmd = rset.getMetaData();
+            int colNum = rsmd.getColumnCount();
+
+            resp.getWriter().write(("\n"));
+
+            while(rset.next()) {
+                for(int n=1; n<=colNum; n++) {
+                    resp.getWriter().write(rset.getString(n) + "\t");
+                }
+                resp.getWriter().write("\n");;
+            }
+
+            resp.getWriter().write("Table End");
+
+            resp.getWriter().write("\n\nPrint Table Complete");
+
+            if(rset!=null){rset.close();}
+            if(s!=null){s.close();}
+        }
+        catch (Exception e) {
+            resp.getWriter().write(e.getMessage());
+
+        }
+
+    }
+    
+    
 }
