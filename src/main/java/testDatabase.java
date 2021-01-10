fb566b857e4974d532da03f3884073444456f6f5
maciej-123 <maciejzajaczkowskiscience@gmail.com>
2021-01-10 16:40:31 +0000

    testdatabase put into class

---
+++ src/main/java/testDatabase.java
@@ -0,0 +1,112 @@
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class testDatabase {
+
+    private Connection c;
+
+    testDatabase(Connection _c) throws IOException
+    {
+        c = _c;
+    }
+
+    public void createTestDatabase(HttpServletResponse resp) throws IOException
+    {
+
+        try {
+            resp.getWriter().write("CreateTestDatabase");
+            Statement s=c.createStatement();
+
+            //select table from INFORMATION_SCHEMA.TABLES - list of all the tables
+            String strSelect = "SELECT * FROM INFORMATION_SCHEMA.TABLES";
+
+            ResultSet rset = s.executeQuery(strSelect);
+
+            if (!rset.next()) {
+                //create test table
+                s.execute("CREATE TABLE label(\n" +
+
+                        "id int PRIMARY KEY NOT NULL," +
+                        "name varchar(45))"
+                );
+            }
+            else
+            {
+                resp.getWriter().write(" Already Created ");
+            }
+
+            resp.getWriter().write("CreateTestDatabase called");
+            if(rset!=null){rset.close();}
+            if(s!=null){s.close();}
+        }
+        catch (Exception e){
+
+            resp.getWriter().write(e.getMessage());
+        }
+    }
+
+    public void returnTestDatabase(HttpServletResponse resp) throws IOException
+    {
+        try {
+            resp.getWriter().write("ReturnTestDatabase");
+            Statement s = c.createStatement();
+            //String strSelect = "SELECT *  FROM INFORMATION_SCHEMA.TABLES";
+            String strSelect = "SELECT * FROM label";
+
+            String transfer1 = new String();
+            String transfer2 = new String();
+
+            resp.getWriter().write(" #1 ");
+            ResultSet rset = s.executeQuery(strSelect);
+            resp.getWriter().write(" #2 ");
+
+            while (rset.next()) {
+                transfer1 += String.valueOf(rset.getInt("id"));
+                transfer2 += rset.getString("name");
+            }
+//
+            resp.getWriter().write(" #3 ");
+            resp.getWriter().write(transfer1);
+            resp.getWriter().write(transfer2);
+
+            resp.getWriter().write("returnTestDatabase called");
+            if(rset!=null){rset.close();}
+            if(s!=null){s.close();}
+        }
+        catch(Exception e)
+        {
+            resp.getWriter().write(e.getMessage());
+        }
+//
+    }
+
+    public void alterTestDatabase(HttpServletResponse resp) throws IOException
+    {
+        try {
+            resp.getWriter().write("AlterTestDatabase");
+            Statement s=c.createStatement();
+
+
+            //ResultSet rset = s.executeQuery(strSelect);
+            resp.getWriter().write(" #2 ");
+
+
+            s.execute("INSERT INTO public.label (id,name) VALUES (1,'2')");
+            s.execute("INSERT INTO public.label (id,name) VALUES (3,'4')");
+
+            resp.getWriter().write("alterTestDatabase called");
+            if(s!=null){s.close();}
+
+        }
+        catch (Exception e){
+
+            resp.getWriter().write(e.getMessage());
+        }
+
+
+    }
+
+}
